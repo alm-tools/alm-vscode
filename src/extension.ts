a7b584cb3d33ea1ab5f3c8f69578e00acf9be118
basarat <basaratali@gmail.com>
2016-07-04 08:40:14 +1000

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -6,19 +6,21 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "alm" is now active!');
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.alm', () => {
         // The code you place here will be executed every time your command is executed
+        const path = vscode.workspace.rootPath;
+        if (!path) {
+            vscode.window.showInformationMessage('You need to have a path open in vscode to launch alm');
+            return;    
+        }
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        // Module to create native browser window.
+        // const electron = require('electron');
+        // const BrowserWindow = require('electron').remote.BrowserWindow;
+        // console.log(BrowserWindow)
     });
 
     context.subscriptions.push(disposable);
